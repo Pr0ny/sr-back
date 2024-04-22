e573844b4d9239557b5aacb8e3e2289ddd5b55ff
Lambert Matthieu <13719880+Pr0ny@users.noreply.github.com>
2024-04-22 17:59:22 +0200

    Local api

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import random
+
+app = Flask(__name__)
+
+tracks = ["track1.mp3", "track2.mp3", "track3.mp3", "track4.mp3", "track5.mp3"]
+
+@app.route('/next_track')
+def next_track():
+    # Get a random track name from the array
+    random_track = random.choice(tracks)
+    # Return the random track name as JSON
+    return jsonify({"next_track": random_track})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0:PORT#")
