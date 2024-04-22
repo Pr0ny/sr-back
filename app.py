49eaef76d149ac0aebe4273fb84000576bd1b038
Lambert Matthieu <13719880+Pr0ny@users.noreply.github.com>
2024-04-22 18:13:23 +0200

    Local api fix debian

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def next_track():
     return jsonify({"next_track": random_track})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0:PORT#")
+    app.run(host="127.0.0.1", port=5000, debug=True)
